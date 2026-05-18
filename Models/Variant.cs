60ffd76edf4c9096f5e27cf10c1a2e9986ccf036
neriogonz <147056665+neriogonz@users.noreply.github.com>
2026-05-18 03:13:02 -0400

    Update Variant.cs

--- Models/Variant.cs
+++ Models/Variant.cs
@@ -1,23 +1,15 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace App_practical.Models // Asegúrate de que sea tu namespace real
+namespace App_practical.Models
 {
     public class Variant
     {
         [Key]
         public int Id { get; set; }
-
-        // Propiedad opcional para guardar el nombre del cálculo
         public string? Name { get; set; }
-
-        // Cambiamos Operand1 por Value1 para que coincida con el servicio Kafka
         public double Value1 { get; set; }
-
-        // Cambiamos Operand2 por Value2
         public double Value2 { get; set; }
-
         public string Operation { get; set; } = string.Empty;
-
         public double Result { get; set; }
     }
 }
