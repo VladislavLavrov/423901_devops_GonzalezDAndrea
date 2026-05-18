54bfb011e2a5805f32fd5a3c6b1c90a49161a467
neriogonz <147056665+neriogonz@users.noreply.github.com>
2026-05-18 03:13:57 -0400

    Update DataViewModel.cs

--- Models/DataViewModel.cs
+++ Models/DataViewModel.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
+
 namespace App_practical.Models
 {
     public class DataViewModel
     {
         public double Result { get; set; }
         public string? ErrorMessage { get; set; }
+        public List<Variant> Variants { get; set; } = new List<Variant>();
     }
 }
